0d82b9c794ada7e4e3c10b6ed147aa9564dfebd2
Stephen Rosen <sirosen@globus.org>
2022-11-01 16:16:51 +0000

    Separate get_collection_list parameter test cases
    Per review, break a monolithic test into several component tests with
    parametrization for their inputs where appropriate.

--- tests/functional/services/gcs/test_get_collection_list.py
+++ tests/functional/services/gcs/test_get_collection_list.py
@@ -8,9 +8,9 @@ def test_get_collection_list(client):
     meta = load_response(client.get_collection_list).metadata
     res = client.get_collection_list()
 
+    # check the results against response metadata to ensure integrity and that
+    # response parsing works
     assert len(list(res)) == len(meta["collection_ids"])
-
-    # sanity check some fields
     assert res["DATA_TYPE"] == "result#1.0.0"
     for index, item in enumerate(res):
         assert item["DATA_TYPE"] == "collection#1.0.0"
@@ -19,37 +19,43 @@ def test_get_collection_list(client):
         assert item["display_name"] == meta["display_names"][index]
 
 
-def test_get_collection_list_include_param(client):
+@pytest.mark.parametrize(
+    "include_param, expected",
+    (
+        (None, None),
+        ("foo", "foo"),
+        ("foo,bar", "foo,bar"),
+        (("foo", "bar"), "foo,bar"),
+    ),
+)
+def test_get_collection_list_include_param(client, include_param, expected):
     load_response(client.get_collection_list)
-
-    client.get_collection_list()
+    client.get_collection_list(include=include_param)
     req = get_last_request()
-    assert "include" not in req.params
+    if include_param is not None:
+        assert "include" in req.params
+        assert req.params["include"] == expected
+    else:
+        assert "include" not in req.params
 
-    client.get_collection_list(include="foo")
-    req = get_last_request()
-    assert "include" in req.params
-    assert req.params["include"] == "foo"
 
-    client.get_collection_list(include="foo,bar")
-    req = get_last_request()
-    assert "include" in req.params
-    assert req.params["include"] == "foo,bar"
-
-    client.get_collection_list(include=("foo", "bar"))
-    req = get_last_request()
-    assert "include" in req.params
-    assert req.params["include"] == "foo,bar"
-
+def test_get_collection_list_mapped_collection_id_param(client):
+    load_response(client.get_collection_list)
     client.get_collection_list(mapped_collection_id="MAPPED_COLLECTION")
     assert get_last_request().params.get("mapped_collection_id") == "MAPPED_COLLECTION"
 
-    filters = ["mapped_collections", "created_by_me"]
-    client.get_collection_list(filter=filters)
-    assert get_last_request().params.get("filter") == ",".join(filters)
 
-    client.get_collection_list(filter="created_by_me")
-    assert get_last_request().params.get("filter") == "created_by_me"
+@pytest.mark.parametrize(
+    "filter_param, expected",
+    (
+        (["mapped_collections", "created_by_me"], "mapped_collections,created_by_me"),
+        ("created_by_me", "created_by_me"),
+    ),
+)
+def test_get_collection_list_filter_param(client, filter_param, expected):
+    load_response(client.get_collection_list)
+    client.get_collection_list(filter=filter_param)
+    assert get_last_request().params.get("filter") == expected
 
 
 def test_error_parsing_forbidden(client):
