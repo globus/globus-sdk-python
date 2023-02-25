6e3fdedd39b9590baf826cd3ab4277ad0f0154ff
Stephen Rosen <sirosen@globus.org>
2023-02-25 00:27:37 +0000

    Allow strings as alternative to Groups enums
    A variety of enums are published by the SDK for use with the Groups
    service. These cover GroupRole, etc and must be imported to use
    various methods in a type-safe way. Make all methods taking these
    enums more permissive by taking a Literal[...] as well. The literals
    are defined alongside the enums for easier consistency between the
    two.
    
    A new type checking test validates these changes against relevant
    usages.
    
    Tests which interact with these Groups API methods now are
    parametrized to pass enum members OR literal strings, variously. These
    must always serialize as strings before being sent.

--- tests/functional/services/groups/test_set_group_policies.py
+++ tests/functional/services/groups/test_set_group_policies.py
@@ -1,5 +1,7 @@
 import json
 
+import pytest
+
 from globus_sdk import (
     GroupMemberVisibility,
     GroupPolicies,
@@ -10,7 +12,40 @@ from globus_sdk._testing import get_last_request
 from tests.common import register_api_route_fixture_file
 
 
-def test_set_group_policies(groups_manager):
+@pytest.mark.parametrize(
+    "group_vis, group_member_vis, signup_fields, signup_fields_str",
+    (
+        (
+            GroupVisibility.private,
+            GroupMemberVisibility.members,
+            [GroupRequiredSignupFields.address1],
+            ["address1"],
+        ),
+        (
+            GroupVisibility.authenticated,
+            GroupMemberVisibility.managers,
+            ["address1"],
+            ["address1"],
+        ),
+        (
+            "private",
+            "members",
+            [GroupRequiredSignupFields.address1, "address2"],
+            ["address1", "address2"],
+        ),
+        ("authenticated", "managers", ["address1"], ["address1"]),
+    ),
+)
+def test_set_group_policies(
+    groups_manager, group_vis, group_member_vis, signup_fields, signup_fields_str
+):
+    group_vis_str = group_vis if isinstance(group_vis, str) else group_vis.value
+    group_member_vis_str = (
+        group_member_vis
+        if isinstance(group_member_vis, str)
+        else group_member_vis.value
+    )
+
     register_api_route_fixture_file(
         "groups",
         "/v2/groups/d3974728-6458-11e4-b72d-123139141556/policies",
@@ -20,10 +55,10 @@ def test_set_group_policies(groups_manager):
     resp = groups_manager.set_group_policies(
         "d3974728-6458-11e4-b72d-123139141556",
         is_high_assurance=False,
-        group_visibility=GroupVisibility.private,
-        group_members_visibility=GroupMemberVisibility.managers,
+        group_visibility=group_vis,
+        group_members_visibility=group_member_vis,
         join_requests=False,
-        signup_fields=[GroupRequiredSignupFields.address1],
+        signup_fields=signup_fields,
         authentication_assurance_timeout=28800,
     )
     assert resp.http_status == 200
@@ -31,12 +66,51 @@ def test_set_group_policies(groups_manager):
     # ensure enums were stringified correctly
     req = get_last_request()
     req_body = json.loads(req.body)
-    assert req_body["group_visibility"] == "private"
-    assert req_body["group_members_visibility"] == "managers"
-    assert req_body["signup_fields"] == ["address1"]
+    assert req_body["group_visibility"] == group_vis_str
+    assert req_body["group_members_visibility"] == group_member_vis_str
+    assert req_body["signup_fields"] == signup_fields_str
 
 
-def test_set_group_policies_explicit_payload(groups_client):
+@pytest.mark.parametrize(
+    "group_vis, group_member_vis, signup_fields, signup_fields_str",
+    (
+        (
+            GroupVisibility.private,
+            GroupMemberVisibility.members,
+            [GroupRequiredSignupFields.address1],
+            ["address1"],
+        ),
+        (
+            GroupVisibility.authenticated,
+            GroupMemberVisibility.managers,
+            ["address1"],
+            ["address1"],
+        ),
+        (
+            "private",
+            "members",
+            [GroupRequiredSignupFields.address1, "address2"],
+            ["address1", "address2"],
+        ),
+        ("authenticated", "managers", ["address1"], ["address1"]),
+    ),
+)
+@pytest.mark.parametrize("setter_usage", (False, "enum", "str"))
+def test_set_group_policies_explicit_payload(
+    groups_client,
+    group_vis,
+    group_member_vis,
+    signup_fields,
+    signup_fields_str,
+    setter_usage,
+):
+    group_vis_str = group_vis if isinstance(group_vis, str) else group_vis.value
+    group_member_vis_str = (
+        group_member_vis
+        if isinstance(group_member_vis, str)
+        else group_member_vis.value
+    )
+
     register_api_route_fixture_file(
         "groups",
         "/v2/groups/d3974728-6458-11e4-b72d-123139141556/policies",
@@ -46,20 +120,26 @@ def test_set_group_policies_explicit_payload(groups_client):
     # same payload as the above test, but formulated without GroupsManager
     payload = GroupPolicies(
         is_high_assurance=False,
-        group_visibility=GroupVisibility.private,
-        group_members_visibility=GroupMemberVisibility.managers,
+        group_visibility=group_vis,
+        group_members_visibility=group_member_vis,
         join_requests=False,
-        signup_fields=[GroupRequiredSignupFields.address1],
+        signup_fields=signup_fields,
         authentication_assurance_timeout=28800,
     )
-    # set a string in the payload directly
-    # this will pass through GroupPolicies.__setitem__
-    payload["group_visibility"] = "authenticated"
+    if setter_usage:
+        # set a string in the payload directly
+        # this will pass through GroupPolicies.__setitem__
+        if setter_usage == "enum":
+            payload["group_visibility"] = group_vis
+        elif setter_usage == "str":
+            payload["group_visibility"] = group_vis_str
+        else:
+            raise NotImplementedError
     # now send it... (but ignore the response)
     groups_client.set_group_policies("d3974728-6458-11e4-b72d-123139141556", payload)
     # ensure enums were stringified correctly, but also that the raw string came through
     req = get_last_request()
     req_body = json.loads(req.body)
-    assert req_body["group_visibility"] == "authenticated"
-    assert req_body["group_members_visibility"] == "managers"
-    assert req_body["signup_fields"] == ["address1"]
+    assert req_body["group_visibility"] == group_vis_str
+    assert req_body["group_members_visibility"] == group_member_vis_str
+    assert req_body["signup_fields"] == signup_fields_str
