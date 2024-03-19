7d426a371ed8b70405ae871a877ac5adec9434cb
Derek Schlabach <derek@globus.org>
2024-03-19 14:11:25 -0500

    Migrate existing scope tests to new path

--- src/globus_sdk/experimental/scope_parser/errors.py
+++
@@ -1,6 +0,0 @@
-class ScopeParseError(ValueError):
-    """The error raised if scope parsing fails."""
-
-
-class ScopeCycleError(ScopeParseError):
-    """The error raised if scope parsing discovers a cycle."""
