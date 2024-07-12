fb52fa693244b3ef7b51453059ce80dbe556bd00
Derek Schlabach <derek@globus.org>
2024-07-12 13:40:32 -0500

    Fix LocalServer on MacOS py3.10 and earlier

---
+++ changelog.d/20240712_133910_derek_fix_local_server_login_on_mac.rst
@@ -0,0 +1,6 @@
+
+Fixed
+~~~~~
+
+- Fixed a GlobusApp bug which would cause LocalServerLoginFlowManager to error on
+  MacOS when versions earlier than python 3.11. (:pr:`NUMBER`)
--- src/globus_sdk/experimental/login_flow_manager/local_server_login_flow_manager.py
+++ src/globus_sdk/experimental/login_flow_manager/local_server_login_flow_manager.py
@@ -56,14 +56,23 @@ def _open_webbrowser(url: str) -> None:
     """
     try:
         browser = webbrowser.get()
-        if browser.name in BROWSER_BLACKLIST:
+        if hasattr(browser, "name"):
+            browser_name = browser.name
+        elif isinstance(browser, webbrowser.MacOSXOSAScript):
+            # MacOSXOSAScript only supports a public name attribute in py311 and later.
+            # https://github.com/python/cpython/issues/82828
+            browser_name = browser._name  # type: ignore[attr-defined]
+        else:
+            raise LocalServerLoginFlowError("Failed to determine browser name.")
+
+        if browser_name in BROWSER_BLACKLIST:
             raise LocalServerLoginFlowError(
                 "Cannot use LocalServerLoginFlowManager with "
-                f"text-only browser '{browser.name}'"
+                f"text-only browser '{browser_name}'"
             )
 
         if not browser.open(url, new=1):
-            raise LocalServerLoginFlowError("Failed to open browser '{browser.name}'")
+            raise LocalServerLoginFlowError(f"Failed to open browser '{browser_name}'")
     except webbrowser.Error as exc:
         raise LocalServerLoginFlowError("Failed to open browser") from exc
 
