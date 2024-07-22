f2b6b8fde555ee535890a8e4c292d88c321ac9f0
Derek Schlabach <derek@globus.org>
2024-07-22 15:16:55 -0500

    Added an 'Updated Example' for GCS Guest Collection Creation

--- docs/experimental/examples/endpoints_and_collections/create_guest_collection/create_guest_collection_user_owned.py
+++ docs/experimental/examples/endpoints_and_collections/create_guest_collection/create_guest_collection_user_owned.py
@@ -3,11 +3,12 @@ from globus_sdk.experimental.globus_app import UserApp
 
 # Tutorial Client ID - <replace this with your own client>
 NATIVE_CLIENT_ID = "61338d24-54d5-408f-a10d-66c06b59f6d2"
-USER_APP = UserApp("my-simple-transfer", client_id=NATIVE_CLIENT_ID)
+USER_APP = UserApp("my-simple-user-collection", client_id=NATIVE_CLIENT_ID)
 
 # Globus Tutorial Collection 1
 # https://app.globus.org/file-manager/collections/6c54cade-bde5-45c1-bdea-f4bd71dba2cc
 ENDPOINT_HOSTNAME = "https://b7a4f1.75bc.data.globus.org"
+STORAGE_GATEWAY_ID = "947460f6-3fcd-4acc-9683-d71e14e5ace1"
 MAPPED_COLLECTION_ID = "6c54cade-bde5-45c1-bdea-f4bd71dba2cc"
 
 
@@ -16,6 +17,8 @@ def main():
 
     # Comment out this line if the mapped collection is high assurance
     attach_data_access_scope(gcs_client, MAPPED_COLLECTION_ID)
+
+    ensure_user_credential(gcs_client)
 
     collection_request = globus_sdk.GuestCollectionDocument(
         public=True,
@@ -39,6 +42,24 @@ def attach_data_access_scope(gcs_client, collection_id):
     manage_collections.add_dependency(data_access)
 
     gcs_client.add_app_scope(manage_collections)
+
+
+def ensure_user_credential(gcs_client):
+    """
+    Ensure that the user has a user credential on the client.
+    This is the mapping between Globus Auth (OAuth2) and the local system's permissions.
+    """
+    try:
+        # Depending on the endpoint & storage gateway, this request document may need to
+        # include more complex information such as a local username.
+        # Consult with the endpoint owner for more detailed info on user mappings and
+        # other particular requirements.
+        req = globus_sdk.UserCredentialDocument(storage_gateway_id=STORAGE_GATEWAY_ID)
+        gcs_client.create_user_credential(req)
+    except globus_sdk.GCSAPIError as err:
+        # If a User Credential already exists; no need to create it.
+        if err.http_status != 409:
+            raise
 
 
 if __name__ == "__main__":
