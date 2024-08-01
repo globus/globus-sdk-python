0ac85e038a7564cffb377dfd3da56a04fd4af8c4
Derek Schlabach <derek@globus.org>
2024-08-01 18:33:59 -0500

    Support ScopeCollectionType in GlobusApp

--- src/globus_sdk/scopes/_normalize.py
+++ src/globus_sdk/scopes/_normalize.py
@@ -11,18 +11,68 @@ if t.TYPE_CHECKING:
 
 def scopes_to_str(scopes: ScopeCollectionType) -> str:
     """
-    Convert a scope collection to a space-separated string.
+    Utility function to normalize a scope collection to a space-separated scope string.
+
+    e.g., scopes_to_str(Scope("foo")) -> "foo"
+    e.g., scopes_to_str(Scope("foo"), "bar", MutableScope("qux")) -> "foo bar qux"
 
     :param scopes: A scope string or object, or an iterable of scope strings or objects.
+    :returns: A space-separated scope string.
     """
-    return " ".join(_iter_scope_collection(scopes))
+    scope_iter = _iter_scope_collection(scopes, split_root_scopes=False)
+    return " ".join(str(scope) for scope in scope_iter)
 
 
-def _iter_scope_collection(obj: ScopeCollectionType) -> t.Iterator[str]:
+def scopes_to_scope_list(scopes: ScopeCollectionType) -> list[Scope]:
+    """
+    Utility function to normalize a scope collection to a list of Scope objects.
+
+    :param scopes: A scope string or object, or an iterable of scope strings or objects.
+    :returns: A list of Scope objects.
+    """
+    scope_list: list[Scope] = []
+    for scope in _iter_scope_collection(scopes, split_root_scopes=True):
+        if isinstance(scope, str):
+            scope_list.extend(Scope.parse(scope))
+        elif isinstance(scope, MutableScope):
+            scope_list.extend(Scope.parse(str(scope)))
+        else:
+            scope_list.append(scope)
+    return scope_list
+
+
+def _iter_scope_collection(
+    obj: ScopeCollectionType,
+    split_root_scopes: bool,
+) -> t.Iterator[str | MutableScope | Scope]:
+    """
+    Convenience function to iterate over a scope collection type.
+
+    Collections of scope representations are yielded one at a time.
+    Individual scope representations are yielded as-is.
+
+    :obj: A scope collection or scope representation.
+    :iter_scope_strings: If True, scope strings with multiple root scopes are split.
+        This flag allows you to skip a bfs operation if merging can be done purely
+        with strings.
+        e.g., _iter_scope_collection("foo bar[baz qux]", True) -> "foo", "bar[baz qux]"
+        e.g., _iter_scope_collection("foo bar[baz qux]", False) -> "foo bar[baz qux]"
+    """
     if isinstance(obj, str):
+        yield from _iter_scope_string(obj, split_root_scopes)
+    elif isinstance(obj, MutableScope) or isinstance(obj, Scope):
         yield obj
-    elif isinstance(obj, (MutableScope, Scope)):
-        yield str(obj)
     else:
         for item in obj:
-            yield str(item)
+            yield from _iter_scope_collection(item, split_root_scopes)
+
+
+def _iter_scope_string(scope_str: str, split_root_scopes: bool) -> t.Iterator[str]:
+    if not split_root_scopes or " " not in scope_str:
+        yield scope_str
+
+    elif "[" not in scope_str:
+        yield from scope_str.split(" ")
+    else:
+        for scope_obj in Scope.parse(scope_str):
+            yield str(scope_obj)
--- tests/unit/scopes/test_scope_normalization.py
+++ tests/unit/scopes/test_scope_normalization.py
@@ -1,6 +1,6 @@
 import pytest
 
-from globus_sdk.scopes import MutableScope, Scope, scopes_to_str
+from globus_sdk.scopes import MutableScope, Scope, scopes_to_scope_list, scopes_to_str
 
 
 def test_scopes_to_str_roundtrip_simple_str():
@@ -35,3 +35,40 @@ def test_scopes_to_str_roundtrip_simple_str_in_collection(scope_collection):
 )
 def test_scopes_to_str_handles_mixed_data(scope_collection, expect_str):
     assert scopes_to_str(scope_collection) == expect_str
+
+
+@pytest.mark.parametrize(
+    "scope_collection",
+    ([Scope("scope1")], Scope("scope1"), "scope1", MutableScope("scope1")),
+)
+def test_scopes_to_scope_list_simple(scope_collection):
+    actual_list = scopes_to_scope_list(scope_collection)
+
+    assert len(actual_list) == 1
+    assert isinstance(actual_list[0], Scope)
+    assert str(actual_list[0]) == "scope1"
+
+
+def test_scopes_to_scope_list_handles_mixed_data():
+    scope_collection = ["scope1", MutableScope("scope2"), Scope.parse("scope3 scope4")]
+    actual_list = scopes_to_scope_list(scope_collection)
+
+    assert len(actual_list) == 4
+    assert all(isinstance(scope, Scope) for scope in actual_list)
+    assert _as_sorted_string(actual_list) == "scope1 scope2 scope3 scope4"
+
+
+def test_scopes_to_scope_list_handles_dependent_scopes():
+    scope_collection = "scope1 scope2[scope3 scope4]"
+    actual_list = scopes_to_scope_list(scope_collection)
+
+    actual_sorted_str = _as_sorted_string(actual_list)
+    # Dependent scope ordering is not guaranteed
+    assert (
+        actual_sorted_str == "scope1 scope2[scope3 scope4]"
+        or actual_sorted_str == "scope1 scope2[scope4 scope3]"
+    )
+
+
+def _as_sorted_string(scope_list) -> str:
+    return " ".join(sorted(str(scope) for scope in scope_list))
