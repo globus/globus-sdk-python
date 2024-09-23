310d965ede001c9467b7dddcbb3872e46ec42d7c
Stephen Rosen <sirosen@globus.org>
2024-09-23 16:56:40 -0500

    Replace a unit test for _consent_client
    This test was checking that we have a non-null _consent_client
    attribute on ValidatingTokenStorage. That attribute no longer exists.
    
    A better and more appropriate test for the default initialization
    path is that we implicitly register the `openid` scope.

--- tests/unit/experimental/globus_app/test_globus_app.py
+++ tests/unit/experimental/globus_app/test_globus_app.py
@@ -157,11 +157,13 @@ def test_user_app_token_storage_configuration(token_storage_value, token_storage
     assert isinstance(user_app._token_storage, token_storage_class)
 
 
-def test_user_app_creates_consent_client():
+def test_user_app_registers_openid_scope_implicitly():
     client_id = "mock_client_id"
     user_app = UserApp("test-app", client_id=client_id)
 
-    assert user_app.token_storage._consent_client is not None
+    assert "auth.globus.org" in user_app.scope_requirements
+    scopes = user_app.scope_requirements["auth.globus.org"]
+    assert "openid" in [str(s) for s in scopes]
 
 
 class MockLoginFlowManager(LoginFlowManager):
