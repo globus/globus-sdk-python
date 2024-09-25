58ff7afa6740fa3574ee54a8297b79670fe0b699
Stephen Rosen <sirosen@globus.org>
2024-09-24 22:54:05 -0500

    Add more unit tests for ValidatingTokenStorage

--- tests/unit/experimental/globus_app/test_validating_token_storage.py
+++ tests/unit/experimental/globus_app/test_validating_token_storage.py
@@ -43,6 +43,72 @@ def _make_memstorage_with_identity_id_validator():
     )
 
 
+def test_validating_token_storage_passes_calls_through():
+    # test that the major methods are called on the inner storage:
+    # - get_token_data
+    # - get_token_data_by_resource_server
+    # - remove_token_data
+    mock_storage = Mock()
+    mock_storage.get_token_data_by_resource_server.return_value = {}
+    instance = ValidatingTokenStorage(mock_storage)
+
+    mock_storage.get_token_data.assert_not_called()
+    instance.get_token_data("foo")
+    mock_storage.get_token_data.assert_called_once_with("foo")
+
+    mock_storage.remove_token_data.assert_not_called()
+    instance.remove_token_data("foo")
+    mock_storage.remove_token_data.assert_called_once_with("foo")
+
+    # small subtlety here:
+    # get_token_data_by_resource_server gets called once on init
+    # so check that after we invoke it, the call count is 2
+    mock_storage.get_token_data_by_resource_server.assert_called_once()
+    instance.get_token_data_by_resource_server()
+    assert len(mock_storage.get_token_data_by_resource_server.mock_calls) == 2
+
+
+def test_validating_token_storage_defaults_to_no_validators():
+    # basic contract test -- there should be no "out of the box" validators
+    instance = ValidatingTokenStorage(MemoryTokenStorage())
+    assert len(instance.validators) == 0
+
+
+def test_validating_token_storage_initially_calls_validators_with_null_identity_id(
+    make_token_response,
+):
+    # assuming there's no initial token data in storage, on the initial store call
+    # there will be an identity_id in the token data but not in the "prior" data slot
+    mock_validator = Mock()
+    instance = ValidatingTokenStorage(
+        MemoryTokenStorage(), validators=(mock_validator,)
+    )
+
+    identity_id = str(uuid.uuid4())
+    instance.store_token_response(make_token_response(identity_id=identity_id))
+
+    mock_validator.before_store.assert_called_once()
+    call_args = mock_validator.before_store.call_args
+    assert len(call_args.kwargs) == 0
+    assert len(call_args.args) == 2
+    token_data, context = call_args.args
+    assert context.prior_identity_id is None
+    assert context.token_data_identity_id == identity_id
+
+
+def test_validators_are_invoked_even_when_retrieving_empty_data():
+    # `get_token-data_by_resource_server` will invoke validators, even if there is no
+    # data available
+    mock_validator = Mock()
+    instance = ValidatingTokenStorage(
+        MemoryTokenStorage(), validators=(mock_validator,)
+    )
+
+    data = instance.get_token_data_by_resource_server()
+    assert data == {}
+    mock_validator.after_retrieve.assert_called_once()
+
+
 def test_validating_token_storage_evaluates_identity_requirements(make_token_response):
     id_a, id_b = str(uuid.uuid4()), str(uuid.uuid4())
     adapter = _make_memstorage_with_identity_id_validator()
